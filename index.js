dcea928b8fc30995f53564bef6225fd3c1172e6c
Yassine-Sebri <yassine.sebri@enetcom.u-sfax.tn>
2022-12-10 20:32:52 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+app.listen(port, ()=> {
+    console.log(`App listening at http://localhost:${port}`)
+} );
+
+app.get('/', function(req, res) {
+    res.send('<h1>It works!</h1>')
+})
